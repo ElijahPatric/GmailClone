47b658ef952bb50d0b298e5a67a41bef23b0c1a2
Elijah Patric <foreverelijah@gmail.com>
2022-11-10 09:38:55 -0600

    Gmail Clone Part 1

---
+++ app/src/main/java/com/example/gmailclone/components/HomeAppBar.kt
@@ -0,0 +1,63 @@
+package com.example.gmailclone.components
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
+import androidx.compose.material.Icon
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.semantics.Role.Companion.Image
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.gmailclone.GmailApp
+import com.example.gmailclone.ui.theme.GmailCloneTheme
+import com.example.gmailclone.R
+
+@Composable
+fun HomeAppBar() {
+    // Box
+    Box(modifier = Modifier.padding(10.dp)) {
+        // Card
+        Card(modifier = Modifier.requiredHeight(50.dp),
+            shape = RoundedCornerShape(10.dp),
+            elevation = 6.dp) {
+            // Row
+            Row(verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(8.dp)) {
+                // Icon
+                Icon(Icons.Default.Menu, "Menu")
+                // Text
+                Text(text = "Search in emails",
+                modifier = Modifier.weight(2.0f))
+                // Image
+                Image(painter = painterResource(id = R.drawable.image),
+                    contentDescription = "Profile",
+                modifier = Modifier.size(30.dp)
+                    .clip(CircleShape)
+                    .background(color = Color.Gray))
+
+            }
+        }
+    }
+}
+
+// Preview
+@Preview
+@Composable
+fun DefaultPreview() {
+    GmailCloneTheme {
+        GmailApp()
+    }
+}
